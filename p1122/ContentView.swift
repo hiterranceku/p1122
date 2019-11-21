8d6d51df450e70963905e9ea4f636e8f3d96338d
chiafishh <chiafishh@gmail.com>
2019-11-22 01:46:22 +0800

    Initial Commit

---
+++ p1122/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  p1122
+//
+//  Created by User08 on 2019/11/22.
+//  Copyright © 2019 ntou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
